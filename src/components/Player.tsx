4b086f0a6fdaefd64750724a381a3d39194e4c71
David Madner <david.madner@gmail.com>
2023-06-25 13:48:53 +0200

    initial commit

---
+++ src/components/Player.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React from "react";
+import ReactPlayer from "react-player";
+
+const AUDIO_NIPPLES = [
+  {
+    label: "HAZUNG",
+    start: 12,
+    end: 16,
+  },
+  {
+    label: "MÜTE",
+    start: 8,
+    end: 12,
+  },
+  { label: "HERR MINISTER", start: 67, end: 68 },
+];
+
+export default function Player({ url }: { url: string }) {
+  const playerRef = React.useRef();
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [info, setInfo] = React.useState(AUDIO_NIPPLES[0]);
+
+  return (
+    <>
+      <div className="w-1 h-1 overflow-hidden">
+        <ReactPlayer
+          playing={isPlaying}
+          ref={playerRef}
+          onProgress={({ playedSeconds }) => {
+            console.log("playedSeconds", playedSeconds);
+            if (playedSeconds > info.end) {
+              setIsPlaying(false);
+            }
+          }}
+          onEnded={() => setIsPlaying(false)}
+          url="https://www.youtube.com/watch?v=P59kknO1wQY"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-5">
+        {AUDIO_NIPPLES.map((nipple) => (
+          <button
+            key={nipple.start}
+            className="p-6 bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold  rounded-full shadow"
+            onClick={() => {
+              playerRef.current.seekTo(nipple.start, "seconds");
+              setInfo(nipple);
+              setIsPlaying(true);
+            }}
+          >
+            {nipple.label}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}

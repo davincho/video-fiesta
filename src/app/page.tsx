c5fd76b798ccf3f640411469931bcdf9665f7b9b
David Madner <david.madner@gmail.com>
2023-06-25 14:09:50 +0200

    chore: ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Player from "./../components/Player";
 
 export default async function Home() {
   return (
-    <main className="min-h-screen sm:p-24 md:p-40 p-4">
-      <h1 className="text-3xl text-center pb-5">Audio Nipple Board</h1>
+    <main className="min-h-screen sm:p-24 md:p-40 p-4 flex flex-col justify-center">
+      <h1 className="text-3xl text-center pb-10">Audio Nipple Board</h1>
       <Player url={""} />
     </main>
   );

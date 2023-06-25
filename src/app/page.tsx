15061df083c05c8f5cd882edcd8e0d84e9c530cb
David Madner <david.madner@gmail.com>
2023-06-25 14:07:00 +0200

    mobile optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Player from "./../components/Player";
 
 export default async function Home() {
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen sm:p-24 md:p-40 p-4">
       <h1 className="text-3xl text-center pb-5">Audio Nipple Board</h1>
       <Player url={""} />
     </main>
